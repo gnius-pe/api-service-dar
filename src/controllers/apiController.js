58c821de80b6c2fa07a062fea280675826e2478d
igor <igor.ramos.c@hotmail.com>
2023-09-24 16:47:13 -0500

    data persona

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,7 +1,11 @@
 // src/controllers/apiController.js
 exports.getData = (req, res) => {
     // Manejar la lógica para obtener datos aquí
-    const data = { mensaje: 'Esto es un ejemplo de datos JSON' };
+    const data = { idPersona: 'Esto es un ejemplo de datos JSON',
+                    nombre : 'Pedro',
+                    apellidos : 'Cantaro J',
+                    edad : 23
+                  };
     res.json(data);
   };
   
