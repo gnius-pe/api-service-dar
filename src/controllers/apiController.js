60998862941cc3423e075c95b5503d4ccfb109d8
igor <igor.ramos.c@hotmail.com>
2023-09-29 11:59:11 -0500

    fallo

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -3,7 +3,7 @@ exports.getData = (req, res) => {
     // Manejar la lógica para obtener datos aquí
     const data = { idPersona: 1,
                     nombre : 'Pedro',
-                    apellidos : 'Cantaro J',
+                    apellidos : 'Cantaro J'
                     edad : 23
                   };
     res.json(data);
