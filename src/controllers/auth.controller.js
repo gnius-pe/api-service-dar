a22e533e948e3eac021cd946d71a64f337015943
igor <igor.ramos.c@hotmail.com>
2023-10-27 19:13:25 -0500

    protection wthi profile in the api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -57,3 +57,18 @@ export const logout = (req,res) => {
     })
     return res.sendStatus(200)
 }
+
+export const profile = async (req,res) => {
+    const userFound = await  User.findById(req.user.id)
+    if(!userFound) return res.status(400).json({
+        message: "User not found"
+    });
+
+    return res.json({
+        id:userFound._id,
+        username : userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    });
+}
