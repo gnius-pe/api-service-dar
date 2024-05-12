ab2326579a546628ed61d5cfe9dbb5c68f421ba9
igorrc <igor.ramos.c@hotmail.com>
2024-05-11 22:26:00 -0500

    merge

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -7,3 +7,11 @@ export const validateschema = (schema) => (req,res,next)  => {
         return res.status(400).json({error : error.errors.map((error)=>error.message)});
     }
 };
+
+export const validateAdmin = (user) => (req,res,next) =>{
+    try {
+        console.log(user);
+    } catch (error) {
+        console.log(errro.message);
+    }
+}
