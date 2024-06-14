2d8f65c15ea5d46d442a53ebb5e5d4c3063b2d4c
igorrc <igor.ramos.c@hotmail.com>
2024-06-14 18:50:10 -0500

    feat : agregar manejo de errores

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ import  {PORT_SERVICE} from "./src/config.js";
 import { swagggerJSDocs } from "./src/libs/swagger.js";
 import figlet from "figlet";
 
-connectDB();
-app.listen(PORT_SERVICE);
-swagggerJSDocs(app,PORT_SERVICE);
-figlet('    Gnius Code',(err,result)=>{
-    console.log(err || result);
-});
+const startServer = async () =>{
+    try {
+        await connectDB();
+        app.listen(PORT_SERVICE,() =>{
+            console.log(`Server is running on port ${PORT_SERVICE}`);
+            swagggerJSDocs(app,PORT_SERVICE);
+        });
+        figlet('    Gnius Code',(err,result) => {
+            if(err){
+                console.error('Error generating ASCII art:', err);
+                return;
+            }
+            console.log(result);
+        })
+    } catch (error) {
+        console.error('Failed to start the server:', error);
+    }
+};
 
-console.log('server on port',PORT_SERVICE);
+startServer();
