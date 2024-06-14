2b9f95edc3ffdcb628519aa4bbfa0feb731d71f4
igorrc <igor.ramos.c@hotmail.com>
2024-06-14 18:26:33 -0500

    refactor : estructur la responsabilidad

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import app from "./src/app.js";
 import {connectDB} from "./src/data/db.js";
 import  {PORT_SERVICE} from "./src/config.js";
+import { swagggerJSDocs } from "./src/libs/swagger.js";
+import figlet from "figlet";
 
 connectDB();
 app.listen(PORT_SERVICE);
+swagggerJSDocs(app,PORT_SERVICE);
+figlet('    Gnius Code',(err,result)=>{
+    console.log(err || result);
+});
 
 console.log('server on port',PORT_SERVICE);
--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ import patientRouter from "./routes/patient.routes.js";
 import verifiNumberRouter from "./routes/verifyNumber.routes.js";
 import misionRouter from "./routes/mision.routes.js";
 import userRouter from "./routes/userManagement.routes.js";
-import {swagggerJSDocs} from "../src/libs/swagger.js";
 import cors from "cors";
-import figlet from "figlet";
 import { CLIENT_URL, DEV_URL} from './config.js';
 
 const app = express();
@@ -17,8 +15,6 @@ app.use(cors({
     origin:[DEV_URL,CLIENT_URL],
     credentials:true
 }));
-
-
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
@@ -27,9 +23,5 @@ app.use('/api',patientRouter);
 app.use('/api/v1',verifiNumberRouter);
 app.use('/api',misionRouter);
 app.use('/api',userRouter);
-swagggerJSDocs(app,3000);
-figlet('    Gnius Code',(err,result)=>{
-    console.log(err || result);
-});
 
 export default app;
