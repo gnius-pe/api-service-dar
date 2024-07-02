48821f4a7b9cd3abd190e0ebfffc6b1525b69500
RENZODeVe <renzo.dejo@unas.edu.pe>
2024-07-02 09:43:22 -0500

    Actualización de puertos para despliegue

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export const URL_BD = process.env.URL;
 export const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 export const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 export const TWILIO_SERVICE_SID = process.env.TWILIO_SERVICE_SID;
-export const PORT_SERVICE = process.env.PORT;
+export const PORT_SERVICE = process.env.PORT || 3000;
 export const CLIENT_URL = process.env.CLIENT_URL;
 export const DEV_URL = process.env.DEV_URL;
