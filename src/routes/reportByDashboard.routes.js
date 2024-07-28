31f453e2f6ccb67c90a2516859ce1e2660f6664c
igorrc <igor.ramos.c@hotmail.com>
2024-07-28 18:51:30 -0500

    feature : add doc report

--- src/routes/reportByDashboard.routes.js
+++ src/routes/reportByDashboard.routes.js
@@ -3,6 +3,17 @@ import {quotasBySpecialty} from "../controllers/reportBySpecialty.controller.js"
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/report/specialty:
+ *  get:
+ *      summary: Obtiene detalles de los cupos por especialidad
+ *      tags:
+ *       - Report
+ *      responses:
+ *          200:
+ *              description: Listado de especialidades
+ */
 router.get('/report/specialty', quotasBySpecialty);
 
 
